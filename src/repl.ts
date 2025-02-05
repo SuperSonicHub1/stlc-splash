488af49832ec9ac30e3eaca5b194153b27760a65
SciDev5 <skhuber0@gmail.com>
2025-02-05 09:36:32 -0500

    Add Repl

---
+++ src/repl.ts
@@ -0,0 +1,39 @@
+import { Expr, Type } from "./ast.ts";
+import { evaluate } from "./eval.ts";
+import { Language } from "./grammar.ts";
+import { solveTypes, typeName } from "./type.ts";
+
+function replIteration(code: string): string {
+
+
+    const parseResult = Language.Parse("Expr", code) as [Expr, string] | []
+    if (parseResult.length === 0 || parseResult[1].trim().length > 0) {
+        return "Syntax error" + (parseResult[1]?.length ?? 0 > 0 ? `: cannot match '...${parseResult[1]}'` : "")
+    }
+    const [ast] = parseResult
+    let type: Type
+    try {
+        type = solveTypes(ast)
+    } catch (e) {
+        return "Type error: " + (e as Error).message
+    }
+    const value = evaluate(ast)
+
+    return `${value} : ${typeName(type)}`
+}
+
+export async function repl() {
+    while (true) {
+        const code = prompt("> ")?.trim()
+        if (code == null || code === "") break
+
+        const result = replIteration(code)
+
+        await Deno.jupyter.broadcast("display_data", {
+            data: { "text/plain": `> ${code}\n  = ${result}` },
+            metadata: {},
+            transient: { display_id: "progress" }
+        })
+    }
+
+}
